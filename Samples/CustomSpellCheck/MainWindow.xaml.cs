3cf701ef4138b6df8509fa761379c1d6c274cc2b
prabakaran.sangameswaran <prabakaran.sangameswaran@syncfusion.com>
2020-06-26 20:50:09 +0530

    Sample MVVM format changed to normal format

--- Samples/CustomSpellCheck/MainWindow.xaml.cs
+++ Samples/CustomSpellCheck/MainWindow.xaml.cs
@@ -1,28 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Syncfusion.SpellChecker.Base;
+using Syncfusion.Windows.Controls;
+using Syncfusion.Windows.Shared;
+using System;
+using System.Globalization;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CustomSpellCheck
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
+    public partial class MainWindow : Window {
+        TextSpellEditor SpellEditor;
+
+        public IEditorProperties Editor {
+            get;
+            set;
+        }
+
+        public SfSpellChecker SpellChecker {
+            get;
+            set;
+        }
         public MainWindow()
         {
+            SpellChecker = new SfSpellChecker();
             InitializeComponent();
+            SpellEditor = new TextSpellEditor(txtbx);
+            Editor = SpellEditor;
+
+            //Creating a culture instance
+            CultureInfo culture = new CultureInfo("en-US");
+
+            // Adding HunSpell dictonaries in Dictionaries collection
+            SpellChecker.Dictionaries = new DictionaryCollection();
+
+            //Add US culture HunSpell dictionary
+            SpellChecker.Dictionaries.Add(
+                new HunspellDictionary()
+                {
+                    Culture = culture,
+                    GrammarUri = new Uri("/CustomSpellCheck;component//English/en-US.aff", UriKind.Relative),
+                    DictionaryUri = new Uri("/CustomSpellCheck;component//English/en-US.dic", UriKind.Relative)
+                }
+            );
+
+            // Get the current PROJECT directory
+            string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
+
+            //Add US culture Custom dictionary
+            SpellChecker.Dictionaries.Add(
+                new CustomDictionary()
+                {
+                    Culture = culture,
+                    DictionaryUri = new Uri(projectDirectory + @"\English\Custom_en-US.txt", UriKind.Absolute)
+                }
+            );
+
+            //Setting a US culture for SpellChecker
+            SpellChecker.Culture = culture;
+            SpellChecker.PerformSpellCheckUsingContextMenu(Editor);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            SpellChecker.PerformSpellCheckUsingDialog(Editor);
         }
     }
 }
