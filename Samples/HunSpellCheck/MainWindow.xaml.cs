495cdb988b7446c52ee5c1e928a59c524cfd5145
prabakaran.sangameswaran <prabakaran.sangameswaran@syncfusion.com>
2020-06-26 20:46:30 +0530

    Samples MVVM pattern reverted

--- Samples/HunSpellCheck/MainWindow.xaml.cs
+++ Samples/HunSpellCheck/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Syncfusion.SpellChecker.Base;
+using Syncfusion.Windows.Controls;
+using Syncfusion.Windows.Shared;
+using System;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace HunSpellCheck
 {
@@ -20,9 +14,51 @@ namespace HunSpellCheck
     /// </summary>
     public partial class MainWindow : Window
     {
+        TextSpellEditor SpellEditor;
+
+        public IEditorProperties Editor
+        {
+            get;
+            set;
+        }
+
+        public SfSpellChecker SpellChecker
+        {
+            get;
+            set;
+        }
         public MainWindow()
         {
+            SpellChecker = new SfSpellChecker();
             InitializeComponent();
+            SpellEditor = new TextSpellEditor(txtbx);
+            Editor = SpellEditor;
+
+            //Creating a culture instance
+            CultureInfo culture = new CultureInfo("fr-FR");
+
+            // Adding HunSpell dictonaries in Dictionaries collection
+            SpellChecker.Dictionaries = new DictionaryCollection();
+
+            //Add French culture HunSpell dictionary
+            SpellChecker.Dictionaries.Add(
+                new HunspellDictionary()
+                {
+                    Culture = culture,
+                    GrammarUri = new Uri("/HunSpellCheck;component//French/fr-FR.aff", UriKind.Relative),
+                    DictionaryUri = new Uri("/HunSpellCheck;component//French/fr-FR.dic", UriKind.Relative)
+                }
+            );
+
+            //Setting a French culture for SpellChecker
+            SpellChecker.Culture = culture;
+            SpellChecker.PerformSpellCheckUsingContextMenu(Editor);
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SpellChecker.PerformSpellCheckUsingDialog(Editor);
         }
     }
 }
--- Samples/OpenOfficeSpellCheck/MainWindow.xaml.cs
+++ Samples/OpenOfficeSpellCheck/MainWindow.xaml.cs
@@ -1,28 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Syncfusion.SpellChecker.Base;
+using Syncfusion.Windows.Controls;
+using Syncfusion.Windows.Shared;
+using System;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace OpenOfficeSpellCheck
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        TextSpellEditor SpellEditor;
+
+        public IEditorProperties Editor {
+            get;
+            set;
+        }
+
+        public SfSpellChecker SpellChecker {
+            get;
+            set;
+        }
+
+        public MainWindow() {
+            SpellChecker = new SfSpellChecker();
             InitializeComponent();
+            SpellEditor = new TextSpellEditor(txtbx);
+            Editor = SpellEditor;
+
+            //Creating a Spanish culture instance
+            CultureInfo culture = new CultureInfo("es-ES");
+
+            SpellChecker = new SfSpellChecker();
+
+            // Adding OpenOffice dictonaries in Dictionaries collection
+            SpellChecker.Dictionaries = new DictionaryCollection();
+
+            //Add Spanish culture OpenOffice dictionary
+            SpellChecker.Dictionaries.Add(
+                new OpenOfficeDictionary()
+                {
+                    Culture = culture,
+                    GrammarUri = new Uri("/OpenOfficeSpellCheck;component//Spanish/es-ES.aff", UriKind.Relative),
+                    DictionaryUri = new Uri("/OpenOfficeSpellCheck;component//Spanish/es-ES.dic", UriKind.Relative)
+                }
+            );
+
+            //Setting a French culture for SpellChecker
+            SpellChecker.Culture = culture;
+            SpellChecker.PerformSpellCheckUsingContextMenu(Editor);
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            SpellChecker.PerformSpellCheckUsingDialog(Editor);
         }
     }
 }
